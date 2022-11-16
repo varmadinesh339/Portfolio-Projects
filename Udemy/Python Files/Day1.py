716e1fce019b4b2b639a49e3a70eaa70c00e2e72
varmadinesh339 <=>
2022-11-16 17:14:22 +1100

    Brand_Generator_v1

---
+++ Udemy/Python Files/Day1.py
@@ -0,0 +1,12 @@
+# This is a Brand name generator Progam
+
+print("Welcome to the Brand Name Generator Program")
+
+# This is a input command used for taking a Name
+_name = input("Please input you Name ? ")
+
+# This is a input command used for taking a Pets Name
+pet_Name = input("Please input your pets Name ? ")
+
+# This is a f string that can be used to display multiple data types
+print(f"One of the following Brand Name could be {pet_Name} {_name}")
