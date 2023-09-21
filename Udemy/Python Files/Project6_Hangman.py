98993be64423b335d37379b246dfe85cf2a7d8a2
varmadinesh339 <varmadinesh339@gmail.com>
2023-09-21 23:01:18 +0530

    Updated program with hangman output

--- Udemy/Python Files/Project6_Hangman.py
+++ Udemy/Python Files/Project6_Hangman.py
@@ -18,16 +18,84 @@ choice = choice.lower()
 for i in range(0,length):
     results.append('_')
     print('_ ' , end = '')
+#-----------------------------------------------------------------------------------
+def hangman_dis(count):
+    if count == 1:
+            print( '''
+            +---+
+            |   |
+                |
+                |
+                |
+                |
+            =========''')
+    elif count ==2:
+        print( '''
+            +---+
+            |   |
+            O   |
+                |
+                |
+                |
+            =========''')
+    elif count == 3:
+        print( '''
+            +---+
+            |   |
+            O   |
+            |   |
+                |
+                |
+            ========='''),
+    elif count == 4:
+        print('''
+            +---+
+            |   |
+            O   |
+           /|   |
+                |
+                |
+            =========''')
+    elif count == 5:
+        print('''
+            +---+
+            |   |
+            O   |
+           /|\  |
+                |
+                |
+            =========''')
+    elif count == 6: 
+        print('''
+            +---+
+            |   |
+            O   |
+           /|\  |
+           /    |
+                |
+            =========''')
+    elif count == 7:
+        print( '''
+            +---+
+            |   |
+            O   |
+           /|\  |
+           / \  |
+                |
+            =========''')
+
+
 
 #------------------------------------------------------------------------------------
 count = 0
 C = Counter(choice)
 nr = 0
-while count < 6 and nr != length:
+while count < 7 and nr != length:
     word = input("Please input your guess: ")
     word = word.lower() 
     if C[word]  == 0 :
         count += 1
+        hangman_dis(count)
     elif C[word] > 0:
         for i in range(0,length):
             if choice[i] == word:
@@ -35,7 +103,8 @@ while count < 6 and nr != length:
                     results[i] = word
                     C[word] -= 1
                     nr += 1
-                    print(results)
+                    for i in range(0,len(results)):
+                        print(results[i], end=' ')
                     break
                 else:
                     continue
