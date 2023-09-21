593fc7dedb0a631ac23bf52d591d2605836661f4
varmadinesh339 <varmadinesh339@gmail.com>
2023-09-21 18:34:07 +0530

    adding \n to improve output

--- Udemy/Python Files/Project3_Treasure Island.py
+++ Udemy/Python Files/Project3_Treasure Island.py
@@ -8,14 +8,13 @@ choice = choice.lower();
 if choice == "r" or choice == "right":
     print("Sorry you fell into a hole and game is over")
 elif choice == "left" or choice == "l" :
-    a ="""You have Reached a lake 
-    Please make a choice between swim or wait for a boat"""
+    a ="""\nYou have Reached a lake Please make a choice between swim or wait for a boat """
     choice =input(a)
     choice = choice.lower()
     if choice == "swim" or choice == "S":
-        print("Sorry the Lake was very lengthy and are dead due to exhaustion. Game Over.")
+        print("\nSorry the Lake was very lengthy and are dead due to exhaustion. Game Over.")
     elif choice == "wait" or choice == "w":
-        print("You have chossen to wait for a boat and then used it to pass the lake")
+        print("\nYou have chossen to wait for a boat and then used it to pass the lake")
 
         a = "After passing the lake you encounter three doors one Yellow, Red and Blue "
         choice = input(a)
